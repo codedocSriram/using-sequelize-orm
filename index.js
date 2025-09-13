c34f15156c778219f4a1204ce1ae0d9ba14cc50a
Sriram Annadurai <42068041+codedocSriram@users.noreply.github.com>
2025-09-14 01:45:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const db = require("./utils/dbConnection");
 const studentRouter = require("./routes/studentsRouter");
+const studentModels = require("./models/students");
 const app = express();
 
 app.use(express.json());
@@ -10,6 +12,13 @@ app.get("/", (req, res) => {
   res.send("Welcome to homePage!!!");
 });
 
-app.listen(4000, () => {
-  console.log("Server running in port 4000");
-});
+db
+  .sync({ force: true })
+  .then(() => {
+    app.listen(4000, () => {
+      console.log("Server is running in port 4000");
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
